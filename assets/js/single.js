b7cdebe4506015a64be005ed6f5486d351f6c20f
LaLacou <108777796+users.noreply.github.com>
2022-08-15 18:57:44 -0700

    updated

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,35 +1,41 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector('#limit-warning');
 
-var getRepoIssues = function(repo) {
-   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+var getRepoIssues = function (repo) {
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-   fetch(apiUrl).then(function(response){
-    if(response.ok) {
-        response.json().then(function(data){
-            displayIssues(data);
-        });
-    }
-    else {
-        alert("There was a problem with your request")
-    }
-   });
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    console.log("repo has more than 30 issues");
+                }
+            });
+        }
+        else {
+            alert("There was a problem with your request")
+        }
+    });
 }
 
-getRepoIssues('llacourciere');
+getRepoIssues('facebook/react');
 
-var displayIssues = function(issues) {
-    if(issues.length === 0) {
+var displayIssues = function (issues) {
+    if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
     }
 
-    for (var i=0; i< issues.length; i++) {
+    for (var i = 0; i < issues.length; i++) {
         var issueEl = document.createElement('a');
         issueEl.classList = 'list-item flex-row justify-space-between align-center';
         issueEl.setAttribute('href', issues[i].html_url);
         issueEl.setAttribute('target', '_blank');
 
         var titleEl = document.createElement('span');
-            titleEl.textContent = issues[i].title;
+        titleEl.textContent = issues[i].title;
         issueEl.appendChild(titleEl);
 
         var typeEl = document.createElement('span')
